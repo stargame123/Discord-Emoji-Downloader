17f9724c5b2ffb5885e00b5964741c05cf99a1af
stargame123 <kumokochanoficial@gmail.com>
2025-11-11 18:46:20 -0500

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+const config: Config = {
+  darkMode: ['class'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
